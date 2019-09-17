14fca4d3af3c27f061911507a96a114891cfc463
Godoy94 <domingues.godoy@hotmail.com.br>
2019-09-17 10:17:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './estilos.css'
+
+import Header from './components/Header';
+import main from './pages/main';
+
+const App = () =>(
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+);
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './estilo.css'
+
+const Header = () => <header id="main-header">JSHunt</header>
+
+export default Header;
